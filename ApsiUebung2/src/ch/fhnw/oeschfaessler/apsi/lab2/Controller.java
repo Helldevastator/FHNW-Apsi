461b0380774c38e8352aac65b45742507d143cfe
fabio-oesch <fabiooesch@hotmail.com>
2013-12-12 16:25:15 +0100

    wfejiopwfejoi

--- ApsiUebung2/src/ch/fhnw/oeschfaessler/apsi/lab2/Controller.java
+++ ApsiUebung2/src/ch/fhnw/oeschfaessler/apsi/lab2/Controller.java
@@ -14,28 +14,31 @@ import javax.servlet.http.HttpServletResponse;
 import ch.fhnw.oeschfaessler.apsi.lab2.model.Company;
 
 public class Controller {
-	
+
 	private static String REGISTER = "rattle_bits/register.jsp";
-	private static String SUCCESS  = "rattle_bits/success.jsp";
-	private static String LOGIN    = "rattle_bits/login.jsp";
-	private static String INDEX    = "rattle_bits/index.jsp";
+	private static String SUCCESS = "rattle_bits/success.jsp";
+	private static String LOGIN = "rattle_bits/login.jsp";
+	private static String INDEX = "rattle_bits/index.jsp";
 	private static String OVERVIEW = "rattle_bits/overview.jsp";
-	
+
 	private final Connection con;
-	
+
 	public Controller(Connection connection) {
 		con = connection;
 	}
-	
-	public void indexPage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	public void indexPage(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher(INDEX).forward(request, response);
 	}
-	
-	public void overviewPage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	public void overviewPage(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO: overview / passwort change page
 	}
-	
-	public void activatePage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	public void activatePage(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		List<String> messages = new ArrayList<>();
 		Company c = new Company(con);
 		boolean activate = false;
@@ -46,22 +49,31 @@ public class Controller {
 		}
 		if (activate) {
 			c.sendLoginData();
-			request.setAttribute("message", "Aktivierung erfolgreich! Sie bekommen ihr Username und Passwort per Mail zugesant.");
+			request.setAttribute(
+					"message",
+					"Aktivierung erfolgreich! Sie bekommen ihr Username und Passwort per Mail zugesant.");
 			request.getRequestDispatcher(SUCCESS).forward(request, response);
 		} else {
-			if (messages.size() == 0) messages.add("activierungs code ist ungültig");
+			if (messages.size() == 0) {
+				messages.add("activierungs code ist ungültig");
+			}
 			request.setAttribute("message", messages.get(0));
 			request.getRequestDispatcher(SUCCESS).forward(request, response);
 		}
 
 	}
-	
-	public void regsiterPage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	public void regsiterPage(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		List<String> messages = new ArrayList<>();
 		Company c = new Company(con);
 		c.setName(request.getParameter("firma"));
 		c.setAddress(request.getParameter("address"));
-		try { c.setZip(Integer.parseInt(request.getParameter("plz"))); } catch (NumberFormatException e) { messages.add("Ungültige PLZ"); }
+		try {
+			c.setZip(Integer.parseInt(request.getParameter("plz")));
+		} catch (NumberFormatException e) {
+			messages.add("Ungültige PLZ");
+		}
 		c.setTown(request.getParameter("town"));
 		c.setMail(request.getParameter("mail"));
 		messages.addAll(c.validate());
@@ -70,7 +82,7 @@ public class Controller {
 		request.setAttribute("plz", String.valueOf(c.getZip()));
 		request.setAttribute("town", c.getTown());
 		request.setAttribute("mail", c.getMail());
-		if (messages.size() > 0 ) {
+		if (messages.size() > 0) {
 			request.setAttribute("messages", messages);
 			request.getRequestDispatcher(REGISTER).forward(request, response);
 		} else {
@@ -80,20 +92,31 @@ public class Controller {
 			} catch (SQLException e) {
 				messages.add("Datenbankverbindung fehlgeschlagen, bitte versuchen sie es später noch einmal");
 				request.setAttribute("messages", messages);
-				request.getRequestDispatcher(REGISTER).forward(request, response);
+				request.getRequestDispatcher(REGISTER).forward(request,
+						response);
 			}
 			c.sendActivationCode();
-			request.setAttribute("message", "Registrierung erfolgreich, bitte warten sie auf den Aktivierungslink per Mail");
+			request.setAttribute("message",
+					"Registrierung erfolgreich, bitte warten sie auf den Aktivierungslink per Mail");
 			request.getRequestDispatcher(SUCCESS).forward(request, response);
 		}
 	}
-	
-	public void loginPage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	public void loginPage(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		if (!request.isSecure()) {
+			String secureAdresse = request.getRequestURL().replace(0, 4,
+					"https")
+					+ "?" + request.getQueryString();
+			System.out.println(secureAdresse);
+			response.sendRedirect(secureAdresse);
+		}
 		List<String> messages = new ArrayList<>();
 		Company c = new Company(con);
 		boolean login = false;
 		try {
-			login = c.checkLogin(request.getParameter("user"), request.getParameter("password"));
+			login = c.checkLogin(request.getParameter("user"),
+					request.getParameter("password"));
 		} catch (SQLException e) {
 			messages.add("Datenbankverbindung fehlgeschlagen, bitte versuchen sie es später noch einmal");
 		}
@@ -102,7 +125,9 @@ public class Controller {
 			request.getSession().setAttribute("username", c.getUsername());
 			request.getRequestDispatcher(OVERVIEW).forward(request, response);
 		} else {
-			if (messages.size() == 0) messages.add("username oder passwort ist ungültig");
+			if (messages.size() == 0) {
+				messages.add("username oder passwort ist ungültig");
+			}
 			request.setAttribute("messages", messages);
 			request.getRequestDispatcher(LOGIN).forward(request, response);
 		}
